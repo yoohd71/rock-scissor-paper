ebf8332a00b77b46f39e3a40f45ed519b8c6f955
yoohd71 <yoohd71@gmail.com>
2023-02-04 13:09:25 -0500

    First Draft

--- javascript.js
+++ javascript.js
@@ -8,13 +8,14 @@ let getComputerChoice = () => {
         return 'paper'
     }
 }
+// Commit 1: changed getcOmputerchoice() function with arrow function for better readbility
 
 let Computer = getComputerChoice();
-console.log(getComputerChoice())
+console.log(Computer)
 
 let User = prompt("What is your choice?").toLowerCase()
 
-function playRound(userSelection, comptuerSleection){
+function playRound(userSelection, computerSelection){
     if (userSelection == computerSelection) {
         return 'Tie'
     } else if ((userSelection == 'rock' && computerSelection == 'paper') || (userSelection == 'scissor' && computerSelection == 'rock') || (userSelection == 'paper' && computerSelection == 'scissor')){
@@ -24,4 +25,27 @@ function playRound(userSelection, comptuerSleection){
     }
 } 
 
-console.log()
+
+function game() {
+    let userScore = 0;
+    let compScore = 0;
+    for (let i = 0; i < 5; i++){
+        let userSelection = User;
+        let computerSelection = Computer;
+        let result = playRound(userSelection, computerSelection);
+        if (result == "Computer"){
+            compScore++;
+        } else if (result =="User"){
+            userScore++;
+        }
+    }
+    if (userScore > compScore){
+        console.log(`You won the computer by ${userScore-compScore} rounds!`);
+    } else if (userScore < compScore){
+        console.log(`Well.. you lost by ${compScore - userScore} rounds`)
+    } else {
+        console.log('TIE')
+    }
+}
+
+console.log(game())
